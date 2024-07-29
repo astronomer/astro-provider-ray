9a1f155be746e9f50b07863be346b1a997163794
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-07-29 14:06:00 -0400

    bug fix

--- scripts/test/unit_cov.sh
+++ scripts/test/unit_cov.sh
@@ -3,4 +3,6 @@ pytest \
     --cov=ray_provider \
     --cov-report=term-missing \
     --cov-report=xml \
-    --durations=0
+    --durations=0 \
+    -m "not (integration or perf)" \
+    --ignore=tests/test_dag_example.py
