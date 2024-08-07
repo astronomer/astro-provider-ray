6aa46cf8c8bad58de51f9071b1627395dff6811b
Venkata Jagannath <160357297+venkatajagannath@users.noreply.github.com>
2024-08-07 00:03:53 -0400

    Adding Integration Tests (#27)
    * initial test
    
    * path changed
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * fix
    
    * update
    
    * bug fix
    
    * bug fix
    
    * update
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * update
    
    * update
    
    * bug fix
    
    * bug fix
    
    * Bug fix
    
    * bug fix
    
    * added more cpus
    
    * using minikube
    
    * updated
    
    * 14 cpus
    
    * GKE cluster
    
    * updated
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * print roles
    
    * update
    
    * update
    
    * test
    
    * test
    
    * update
    
    * minor update
    
    * bug fix
    
    * more tests
    
    * update
    
    * bug fix
    
    * update
    
    * update
    
    * try again
    
    * update
    
    * updates
    
    * update
    
    * try again
    
    * node svc account set
    
    * update
    
    * update
    
    * update
    
    * update
    
    * yaml files updated
    
    * making ip or dns generic choice
    
    * unit tests added
    
    * minor updates

--- scripts/test/integration_test.sh
+++ scripts/test/integration_test.sh
@@ -1,8 +1,8 @@
-pytest \
-    -vv \
+pytest -vv \
     --cov=ray_provider \
     --cov-report=term-missing \
     --cov-report=xml \
     --durations=0 \
-    -m "not (integration or perf)" \
-    --ignore=tests/test_dag_example.py
+    -m integration \
+    -s \
+    --log-cli-level=DEBUG
