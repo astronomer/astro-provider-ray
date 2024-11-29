1a523c4ad2e423592bca45990500140a28b35a88
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-11-29 08:58:54 +0000

    Add support ot dynamic config

--- ray_provider/decorators.py
+++ ray_provider/decorators.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 import inspect
 import os
 import re
-import shutil
+import tempfile
 import textwrap
-from tempfile import mkdtemp
+from datetime import timedelta
+from pathlib import Path
 from typing import Any, Callable
 
 from airflow.decorators.base import DecoratedOperator, TaskDecorator, task_decorator_factory
-from airflow.exceptions import AirflowException
 from airflow.utils.context import Context
 
 from ray_provider.operators import SubmitRayJob
@@ -28,10 +28,27 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
     """
 
     custom_operator_name = "@task.ray"
+    _config: dict[str, Any] | Callable[..., dict[str, Any]] = dict()
 
     template_fields: Any = (*SubmitRayJob.template_fields, "op_args", "op_kwargs")
 
-    def __init__(self, config: dict[str, Any], **kwargs: Any) -> None:
+    def __init__(self, config: dict[str, Any] | Callable[..., dict[str, Any]], **kwargs: Any) -> None:
+        self._config = config or {}
+        self.kwargs = kwargs
+        super().__init__(conn_id="", entrypoint="python script.py", runtime_env={}, **kwargs)
+
+    def _build_config(self, context: Context) -> dict[str, Any]:
+        if callable(self._config):
+            config_params = inspect.signature(self._config).parameters
+            config_kwargs = {k: v for k, v in self.kwargs.items() if k in config_params and k != "context"}
+            if "context" in config_params:
+                config_kwargs["context"] = context
+            config = self._config(**config_kwargs)
+            assert isinstance(config, dict)
+            return config
+        return self._config
+
+    def _load_config(self, config: dict[str, Any]) -> None:
         self.conn_id: str = config.get("conn_id", "")
         self.is_decorated_function = False if "entrypoint" in config else True
         self.entrypoint: str = config.get("entrypoint", "python script.py")
@@ -39,9 +56,9 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
 
         self.num_cpus: int | float = config.get("num_cpus", 1)
         self.num_gpus: int | float = config.get("num_gpus", 0)
-        self.memory: int | float = config.get("memory", None)
-        self.ray_resources: dict[str, Any] | None = config.get("resources", None)
-        self.ray_cluster_yaml: str | None = config.get("ray_cluster_yaml", None)
+        self.memory: int | float = config.get("memory", 1)
+        self.ray_resources: dict[str, Any] | None = config.get("resources")
+        self.ray_cluster_yaml: str | None = config.get("ray_cluster_yaml")
         self.update_if_exists: bool = config.get("update_if_exists", False)
         self.kuberay_version: str = config.get("kuberay_version", "1.0.0")
         self.gpu_device_plugin_yaml: str = config.get(
@@ -50,35 +67,19 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
         )
         self.fetch_logs: bool = config.get("fetch_logs", True)
         self.wait_for_completion: bool = config.get("wait_for_completion", True)
-        job_timeout_seconds: int = config.get("job_timeout_seconds", 600)
+        job_timeout_seconds = config.get("job_timeout_seconds", 600)
+        self.job_timeout_seconds: timedelta | None = (
+            timedelta(seconds=job_timeout_seconds) if job_timeout_seconds > 0 else None
+        )
         self.poll_interval: int = config.get("poll_interval", 60)
-        self.xcom_task_key: str | None = config.get("xcom_task_key", None)
+        self.xcom_task_key: str | None = config.get("xcom_task_key")
+
         self.config = config
 
         if not isinstance(self.num_cpus, (int, float)):
             raise TypeError("num_cpus should be an integer or float value")
         if not isinstance(self.num_gpus, (int, float)):
             raise TypeError("num_gpus should be an integer or float value")
-
-        super().__init__(
-            conn_id=self.conn_id,
-            entrypoint=self.entrypoint,
-            runtime_env=self.runtime_env,
-            num_cpus=self.num_cpus,
-            num_gpus=self.num_gpus,
-            memory=self.memory,
-            resources=self.ray_resources,
-            ray_cluster_yaml=self.ray_cluster_yaml,
-            update_if_exists=self.update_if_exists,
-            kuberay_version=self.kuberay_version,
-            gpu_device_plugin_yaml=self.gpu_device_plugin_yaml,
-            fetch_logs=self.fetch_logs,
-            wait_for_completion=self.wait_for_completion,
-            job_timeout_seconds=job_timeout_seconds,
-            poll_interval=self.poll_interval,
-            xcom_task_key=self.xcom_task_key,
-            **kwargs,
-        )
 
     def execute(self, context: Context) -> Any:
         """
@@ -88,15 +89,17 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
         :return: The result of the Ray job execution.
         :raises AirflowException: If job submission fails.
         """
-        tmp_dir = None
-        try:
+        config = self._build_config(context)
+        self.log.info(f"Using the following config {config}")
+        self._load_config(config)
+
+        with tempfile.TemporaryDirectory(prefix="ray_") as tmpdirname:
+            temp_dir = Path(tmpdirname)
+
             if self.is_decorated_function:
                 self.log.info(
                     f"Entrypoint is not provided, is_decorated_function is set to {self.is_decorated_function}"
                 )
-                # Create a temporary directory that won't be immediately deleted
-                temp_dir = mkdtemp(prefix="ray_")
-                script_filename = os.path.join(temp_dir, "script.py")
 
                 # Get the Python source code and extract just the function body
                 full_source = inspect.getsource(self.python_callable)
@@ -110,6 +113,7 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
                 call_str = f"{self.python_callable.__name__}({args_str}, {kwargs_str})"
 
                 # Write the script with function definition and call
+                script_filename = os.path.join(temp_dir, "script.py")
                 with open(script_filename, "w") as file:
                     file.write(function_body)
                     file.write(f"\n\n# Execute the function\n{call_str}\n")
@@ -122,21 +126,23 @@ class _RayDecoratedOperator(DecoratedOperator, SubmitRayJob):
             result = super().execute(context)
 
             return result
-        except Exception as e:
-            self.log.error(f"Failed during execution with error: {e}")
-            raise AirflowException("Job submission failed") from e
-        finally:
-            if tmp_dir and os.path.exists(tmp_dir):
-                shutil.rmtree(tmp_dir)
 
     def _extract_function_body(self, source: str) -> str:
         """Extract the function, excluding only the ray.task decorator."""
         lines = source.split("\n")
+        # TODO: Review the current approach, that is quite hacky.
+        # It feels a mistake to have a user-facing module named the same as the official ray SDK.
+        # In particular, the decorator is working in a very artificial way, where ray means two different things
+        # at the scope of the task definition (Astro Ray Provider decorator) and inside the decorated method (Ray SDK)
         # Find the line where the ray.task decorator is
+        # Additionally, if users imported the ray decorator as "from ray_provider.decorators import ray as ray_decorator
+        # The following will stop working.
         ray_task_line = next((i for i, line in enumerate(lines) if re.match(r"^\s*@ray\.task", line.strip())), -1)
 
         # Include everything except the ray.task decorator line
         body = "\n".join(lines[:ray_task_line] + lines[ray_task_line + 1 :])
+        self.log.info(r"Ray job that is going to be executed: \m %s", body)
+
         # Dedent the body
         return textwrap.dedent(body)
 
@@ -146,6 +152,7 @@ class ray:
     def task(
         python_callable: Callable[..., Any] | None = None,
         multiple_outputs: bool | None = None,
+        config: dict[str, Any] | Callable[[], dict[str, Any]] | None = None,
         **kwargs: Any,
     ) -> TaskDecorator:
         """
@@ -153,12 +160,15 @@ class ray:
 
         :param python_callable: The callable function to decorate.
         :param multiple_outputs: If True, will return multiple outputs.
+        :param config: A dictionary of configuration or a callable that returns a dictionary.
         :param kwargs: Additional keyword arguments.
         :return: The decorated task.
         """
+        config = config or {}
         return task_decorator_factory(
             python_callable=python_callable,
             multiple_outputs=multiple_outputs,
             decorated_operator_class=_RayDecoratedOperator,
+            config=config,
             **kwargs,
         )
